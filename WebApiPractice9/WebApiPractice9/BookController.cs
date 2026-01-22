c2a57bb3f89cda3ef42c6914e0234cf6b677f1f2
Viththiyakaran <battikaran6@gmail.com>
2026-01-22 01:01:06 +0000

    DB connect change code

--- WebApiPractice9/WebApiPractice9/BookController.cs
+++ WebApiPractice9/WebApiPractice9/BookController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApiPractice9.Data;
 using WebApiPractice9.Models;
 
 namespace WebApiPractice9
@@ -8,23 +11,30 @@ namespace WebApiPractice9
     [ApiController]
     public class BookController : ControllerBase
     {
-        static private List<Models.Book> books = new List<Models.Book>
+        //static private List<Models.Book> books = new List<Models.Book>
+        //{
+        //    new Models.Book { Id = 1, Title = "Book One", Author = "Author A", Price = 9.99M },
+        //    new Models.Book { Id = 2, Title = "Book Two", Author = "Author B", Price = 14.99M },
+        //    new Models.Book { Id = 3, Title = "Book Three", Author = "Author C", Price = 19.99M }
+        //};
+
+        private readonly AppDbContext _context;
+
+        public BookController(AppDbContext context)
         {
-            new Models.Book { Id = 1, Title = "Book One", Author = "Author A", Price = 9.99M },
-            new Models.Book { Id = 2, Title = "Book Two", Author = "Author B", Price = 14.99M },
-            new Models.Book { Id = 3, Title = "Book Three", Author = "Author C", Price = 19.99M }
-        };
+            _context = context;
+        }
 
         [HttpGet]
-        public ActionResult<List<Models.Book>> GetBooks()
+        public async Task<ActionResult<List<Models.Book>>> GetBooks()
         {
-            return Ok(books);
+            return Ok(await _context.Books.ToListAsync());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<List<Models.Book>> GetBookById(int id)
+        public async Task<ActionResult<List<Models.Book>>> GetBookById(int id)
         {
-            var book = books.FirstOrDefault(b => b.Id == id);
+            var book = await _context.Books.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
@@ -33,42 +43,48 @@ namespace WebApiPractice9
 
         }
         [HttpPost]
-        public ActionResult<Book> AddBook(Book newBook)
+        public async Task<ActionResult<Book>> AddBook(Book newBook)
         {
             if (newBook == null)
             {
                 return BadRequest();
             }
 
-            books.Add(newBook);
+            _context.Books.Add(newBook);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
         }
         [HttpPut("{id}")]
-        public IActionResult UpdateBook(int id, Book updatedBook)
+        public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
         {
-            var existingBook = books.FirstOrDefault(b => b.Id == id);
-            if (existingBook == null)
-            {
-                return NotFound();
-            }
-            existingBook.Title = updatedBook.Title;
-            existingBook.Author = updatedBook.Author;
-            existingBook.Price = updatedBook.Price;
-            //existingBook.Id = updatedBook.Id;
-            return NoContent();
-            
-
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult DeleteBook(int id)
-        {
-            var book = books.FirstOrDefault(b => b.Id == id);
+            var book = await _context.Books.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
             }
-            books.Remove(book);
+
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Price = updatedBook.Price;
+            //existingBook.Id = updatedBook.Id;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
             return NoContent();
         }
     }
