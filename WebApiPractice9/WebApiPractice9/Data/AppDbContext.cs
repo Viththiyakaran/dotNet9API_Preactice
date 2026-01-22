435b53ad108f07f417031655920bb79ae3bdca91
Viththiyakaran <battikaran6@gmail.com>
2026-01-22 00:42:00 +0000

    DB changes

--- WebApiPractice9/WebApiPractice9/Data/AppDbContext.cs
+++ WebApiPractice9/WebApiPractice9/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using WebApiPractice9.Models;
 
 namespace WebApiPractice9.Data
 {
@@ -7,6 +9,16 @@ namespace WebApiPractice9.Data
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
+           
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .Property(b => b.Price)
+                .HasColumnType("decimal(18,2)"); // sets SQL type explicitly
         }
     }
 }
--- WebApiPractice9/WebApiPractice9/Program.cs
+++ WebApiPractice9/WebApiPractice9/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiPractice9.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
@@ -7,6 +13,8 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
 
 app.UseHttpsRedirection();
 
