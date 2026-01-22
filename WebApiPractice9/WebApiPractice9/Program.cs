38046ebc99bb977524df7a3ce0ce63519a4098a8
Viththiyakaran <battikaran6@gmail.com>
2026-01-22 01:33:28 +0000

    Cors orgian for react

--- WebApiPractice9/WebApiPractice9/Program.cs
+++ WebApiPractice9/WebApiPractice9/Program.cs
@@ -1,20 +1,39 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using WebApiPractice9.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Add CORS policybuilder.Services.AddCors(options =>
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+
+
+builder.Services.AddControllers();
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 
+// Use CORS
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
